c04877ea48d84b43d5ce12f65cb3fbda9ef97778
David Lac <kiddmit3@gmail.com>
2017-09-27 22:40:08 -0700

    something to show

--- src/components/Tutor.js
+++ src/components/Tutor.js
@@ -6,8 +6,31 @@ class Tutor extends Component {
   
   render() {
     return (
-        <div className="tutor">
+        <div className="tutor center">
+        <div id="index-banner" className="parallax-container">
+
+
+
+     <div className="parallax"><img src="./img/burden.jpg" alt="Unsplashed background img 1"></img></div>
+    </div>
+        <br></br><br></br>
+            <div className="row">
+            <div className="col s7 m7">
+
+            </div>
+            <div className="col s4 m4">
+            <button data-target="modal1" className="btn waves-effect waves-light red lighten-1 modal-trigger hoverable">ADD</button>
+
+            <button data-target="modal1" className="btn waves-effect waves-light red lighten-1 modal-trigger hoverable">LOGOUT</button></div>
+            </div>
+             <br></br>
+        	<div className="row">
+        	<div className="col s2 m2"></div>
+        	<div className="col s8 m8">
                 <TutorCalendar />
+        </div>
+        <div className="col s2 m2"></div>
+        </div>
         </div>
     );
   }
