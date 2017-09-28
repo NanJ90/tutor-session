afe5270411171ea13ae94d417d17f9e8ea7d1c86
Kyle Serrecchia <kyserrecchia@gmail.com>
2017-09-28 15:09:02 -0700

    more routes!

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ require("./controllers/tutorsubject.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
