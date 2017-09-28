92a650f1d2ba5117409883136f9d7f9bcefd4287
David Lac <kiddmit3@gmail.com>
2017-09-28 12:58:00 -0700

    for safe

--- src/components/Student.js
+++ src/components/Student.js
@@ -19,9 +19,9 @@ class Student extends Component {
 
             </div>
             <div className="col s4 m4">
-            <button data-target="modal1" className="btn waves-effect waves-light red lighten-1 modal-trigger hoverable">ADD</button>
+            <button className="btn waves-effect waves-light red lighten-1 modal-trigger hoverable"><i className="large material-icons">search</i></button>
 
-            <button data-target="modal1" className="btn waves-effect waves-light red lighten-1 modal-trigger hoverable">LOGOUT</button></div>
+            <a href="/"><button className="btn waves-effect waves-light red lighten-1 modal-trigger hoverable">LOGOUT</button></a></div>
             </div>
              <br></br>
             <div className="row">
